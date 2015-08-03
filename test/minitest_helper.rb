fea77f27f38e24e11d5ef51e4ca2771c10aa97c3
Tymon Tobolski <i@teamon.eu>
2015-08-04 00:47:24 +0200

    Minutest reporters and hooks setup

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -3,3 +3,8 @@ require 'hodor'
 
 require 'minitest/pride'
 require 'minitest/autorun'
+require "minitest/reporters"
+require 'minitest/hooks/test'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
